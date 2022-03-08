c8adf81ef11f214feac84cc2782dbf6223d841c2
kimsoldev <sol.k.0618@gmail.com>
2022-03-07 23:47:58 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,17 @@
 
 import React from 'react';
 
+function Potato(){
+  return <h1>I like potato</h1>;
+}
 
 function App() {
-  return <div>hello REACT</div>;
+  return (
+  <div>
+    <h1>Hello</h1>
+    <Potato />
+  </div>
+  );
 }
 
 export default App;
