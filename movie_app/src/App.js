01ed60eeeb723de11d78d61973453fdbbfe416c4
kimsoldev <sol.k.0618@gmail.com>
2022-03-08 23:57:35 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,15 +1,16 @@
 
 import React from 'react';
 
-function Potato(){
-  return <h1>I like potato</h1>;
+function Food(props){
+  console.log(props);
+  return <h1>{ props.fav }</h1>;
 }
 
 function App() {
   return (
   <div>
     <h1>Hello</h1>
-    <Potato />
+    <Food fav="kimchi" />
   </div>
   );
 }
