625d51ad2674313a0422293158a75eba7b0fe9cb
kimsoldev <sol.k.0618@gmail.com>
2022-03-09 23:59:29 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,34 +1,40 @@
 
 import React from 'react';
+import { render } from 'react-dom';
 
 function Food({name, picture}){
   return (
   <div>
     <h1>I like { name }</h1>
-    <img src= { picture } />  
+    <img src= { picture } art={ name }/>  
   </div>
 );
 }
 
 const foodILIke= [
   {
+    id:1,
     name: 'Kimchi',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
   },
   {
+    id:2,
     name: 'Samgyeopsal',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
   },
   {
+    id:3,
     name: 'Doncasu',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
   },  
 ];
 
+
 function App() {
+
   return (
   <div>
-    {foodILIke.map(dish => (<Food name={dish.name} picture={dish.image} />))}
+    {foodILIke.map(dish => ( <Food nmae={dish.name} picture={dish.image} key={dish.id}/>))}
   </div>
   );
 }
