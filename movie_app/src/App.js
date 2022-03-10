38cc5e089536706e81543b7926a048e659de8d14
kimsoldev <sol.k.0618@gmail.com>
2022-03-09 21:05:33 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,16 +1,34 @@
 
 import React from 'react';
 
-function Food(props){
-  console.log(props);
-  return <h1>{ props.fav }</h1>;
+function Food({name, picture}){
+  return (
+  <div>
+    <h1>I like { name }</h1>
+    <img src= { picture } />  
+  </div>
+);
 }
+
+const foodILIke= [
+  {
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
+  },
+  {
+    name: 'Doncasu',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
+  },  
+];
 
 function App() {
   return (
   <div>
-    <h1>Hello</h1>
-    <Food fav="kimchi" />
+    {foodILIke.map(dish => (<Food name={dish.name} picture={dish.image} />))}
   </div>
   );
 }
