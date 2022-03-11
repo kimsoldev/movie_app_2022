f717660f043edb703d87a5074d2bd250ea0b00d2
kimsoldev <sol.k.0618@gmail.com>
2022-03-10 23:42:25 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
-import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
-function Food({name, picture}){
+function Food({name, picture, rating }){
   return (
   <div>
     <h1>I like { name }</h1>
+    <h4>{rating}/5.0np</h4>
     <img src= { picture } art={ name }/>  
+    
   </div>
 );
 }
@@ -16,16 +18,19 @@ const foodILIke= [
     id:1,
     name: 'Kimchi',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
+    rating: 5,
   },
   {
     id:2,
     name: 'Samgyeopsal',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
+    rating: 4,
   },
   {
     id:3,
     name: 'Doncasu',
     image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
+    rating: 3.5,
   },  
 ];
 
@@ -34,9 +39,15 @@ function App() {
 
   return (
   <div>
-    {foodILIke.map(dish => ( <Food nmae={dish.name} picture={dish.image} key={dish.id}/>))}
+    {foodILIke.map(dish => ( <Food name={dish.name} picture={dish.image} key={dish.id} rating={dish.rating} />))}
   </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  picture: PropTypes.string
+};
 
 export default App;
