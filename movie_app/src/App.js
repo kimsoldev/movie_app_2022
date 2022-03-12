b8b4faf3edb03bcc0f28e3e87c21ee05bef0cedd
kimsoldev <sol.k.0618@gmail.com>
2022-03-11 23:54:19 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,53 +1,10 @@
 
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food({name, picture, rating }){
-  return (
-  <div>
-    <h1>I like { name }</h1>
-    <h4>{rating}/5.0np</h4>
-    <img src= { picture } art={ name }/>  
-    
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return <h1>I'm a class component</h1>;
+  }
 }
-
-const foodILIke= [
-  {
-    id:1,
-    name: 'Kimchi',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
-    rating: 5,
-  },
-  {
-    id:2,
-    name: 'Samgyeopsal',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
-    rating: 4,
-  },
-  {
-    id:3,
-    name: 'Doncasu',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-/jpg',
-    rating: 3.5,
-  },  
-];
-
-
-function App() {
-
-  return (
-  <div>
-    {foodILIke.map(dish => ( <Food name={dish.name} picture={dish.image} key={dish.id} rating={dish.rating} />))}
-  </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  picture: PropTypes.string
-};
 
 export default App;
