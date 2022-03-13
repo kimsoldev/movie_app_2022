f4332075bd6f3aab1ec747f76058ba95021877de
kimsoldev <sol.k.0618@gmail.com>
2022-03-12 23:57:23 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,29 @@
 
 import React from 'react';
 
+
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    console.log('add');
+  };
+
+  minus = () => {
+    console.log('minus');
+  };
+
+
   render() {
-    return <h1>I'm a class component</h1>;
+    return (
+    <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    );
   }
 }
 
