aa9e6fc4d08b2053329ef9e34e3026ebc351adc7
kimsoldev <sol.k.0618@gmail.com>
2022-03-17 02:51:24 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,36 +3,13 @@ import React from 'react';
 
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  console.log('hello');
-}
-
   state = {
-    count: 0,
+    isLoading: true,
   };
-
-  add = () => {
-    this.setState( current => ({
-      count: current.count +1
-    }))
-  };
-
-  minus = () => {
-    this.setState( current => ({
-      count: current.count -1
-    }))
-  };
-
 
   render() {
-    return (
-    <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-    );
+    const { isLoading } = this.state;
+    return <div> {isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
