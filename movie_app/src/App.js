141184865645ac0cd1b3f88d54732614d67bf8d0
kimsoldev <sol.k.0618@gmail.com>
2022-03-26 23:57:16 -1200

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -9,9 +9,12 @@ class App extends React.Component {
     isLoading: true,
     movies: [],  
   };
+  getMovies = () => {
+    const movies = axios.get('https://yts-proxy.now.sh/list_movies.json');
+  }
 
   componentDidMount() {
-    axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.getMovies();
   }
 
   render() {
